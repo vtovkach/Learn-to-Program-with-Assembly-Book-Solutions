470dc8270c1a82667c10789a22b3a4bb5829bca2
Sylvester Stallone <vadim003600@gmail.com>
2025-11-24 19:57:20 -0700

    Chapter 6 Exercise 6

---
+++ Chapter 6/ex6/ex6.s
@@ -0,0 +1,43 @@
+# Perform Linear Search to find a target number and return index 
+
+.globl _start 
+
+.section .data 
+target:
+    .quad 155
+
+array_size: 
+    .quad 10
+
+array: 
+    .quad 10, 23, 1, 2, 16, 17, 19, 40, 57, 12
+
+.section .text
+_start:
+    movq target, %rax
+    leaq array, %rbx
+    xorq %rcx, %rcx 
+    movq array_size, %rdx  
+    movq $-1, %rdi 
+
+    cmp %rcx, array_size
+    je end 
+
+mainloop:
+    cmp %rax, (%rbx, %rcx, 8)
+    je target_found 
+    inc %rcx 
+    jmp loop_cont
+
+target_found:
+    movq %rcx, %rdi 
+    jmp end  
+
+loop_cont: 
+    cmp %rdx, %rcx 
+    jb mainloop
+ 
+end:
+    movq $60, %rax 
+    syscall 
+    
