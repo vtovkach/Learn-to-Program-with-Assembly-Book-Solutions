f35e00b2b4bafc58f22d94ae7843d2b1c4ea5988
vtovkach <vadim003600@gmail.com>
2026-01-26 22:46:39 -0600

    started exercise 3

--- extra-practice/ex3.s
+++ extra-practice/ex3.s
@@ -1,2 +1,26 @@
 
 # Given an array of integers and its size, count and store in memory the number of positive and negative elements in the array.
+
+.globl _start 
+
+.section .data
+beg_array:
+    .quad 55, -67, 17, 18, -19, 20, 32, -77, -1233, 8934
+end_array:
+
+.section .bss
+positives:  
+    .skip 8
+negatives: 
+    .skip 8
+
+.equ ARRAY_SIZE, (end_array - beg_array) / 8
+
+.section .text
+_start:
+
+end:
+
+    movq $60, %rax 
+    syscall 
+
